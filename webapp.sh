af5a66d5cb0186ac6313aa26364b8b99b8659d96
Andrews Cordolino Sobral <andrewssobral@users.noreply.github.com>
2023-03-05 11:56:27 +0100

    Update webapp.sh

--- webapp.sh
+++ webapp.sh
@@ -2,4 +2,8 @@
 # first build the virtualenv using the virtualenv.sh script 
 #
 # gradio webapp.py
-CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node $MP webapp.py
+torchrun --nproc_per_node $MP webapp.py
+#
+# or use CUDA_VISIBLE_DEVICES if you want to target a specific gpu device
+# CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node $MP webapp.py
+#
